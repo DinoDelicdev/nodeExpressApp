de8d740ee1eceb2d3536936d9fdb580f98b18f9e
Dino Delic <dino.delic.dev@gmail.com>
2022-12-12 00:17:36 +0100

    Made first node server

---
+++ app.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    console.log(req.url, req.method, req.headers);
+    //process.exit();
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My first page</title></head>');
+    res.write('<body><h1>My first page</h1</body>');
+    res.write('</html>');
+});
+
+server.listen(3000);
